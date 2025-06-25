905c9dc6d916f81f06cf4e3fb6b16af50ab7616d
SanmayChavan <153537885+SanmayChavan@users.noreply.github.com>
2025-06-25 13:19:30 +0530

    Update index.js

--- index.js
+++ index.js
@@ -11,11 +11,6 @@ app.use(express.urlencoded({ extended: false }))
 // routes
 app.use("/api/products", productRoute)
 
-
-
-
-
-
 mongoose.connect("mongodb+srv://sanmaychavan22:sanmaychavan22@backenddb.infmcyj.mongodb.net/Node-API?retryWrites=true&w=majority&appName=BackendDB")
     .then(() => {
         console.log('connected to database');
